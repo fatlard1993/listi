1be99d4ab4feddb763eb4c22f8ea62c0d5e9564d
justfatlard <fatlard1993@gmail.com>
2020-12-02 23:22:38 -0800

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+const yargs = require('yargs');
+const rootFolder = require('find-root')(__dirname);
+
+function rootPath(){ return path.join(rootFolder, ...arguments); }
+
+process.chdir(rootFolder);
+
+yargs.parserConfiguration({
+	'camel-case-expansion': false
+});
+
+yargs.alias({
+	h: 'help',
+	v: 'verbosity',
+	ver: 'version',
+	p: 'port',
+	P: 'persistent'
+});
+
+yargs.boolean(['h', 'ver', 'persistent']);
+
+yargs.default({
+	v: 1,
+	p: 8080,
+	P: true
+});
+
+yargs.describe({
+	h: 'This',
+	v: '<level>',
+	p: '<port>',
+	P: 'Saves lists to file'
+});
+
+const args = yargs.argv;
+
+['_', '$0', 'v', 'p', 'P'].forEach((item) => { delete args[item]; });
+
+const opts = Object.assign(args, { args: Object.assign({}, args), rootFolder, verbosity: Number(args.verbosity) });
+
+const log = new (require('log'))({ tag: 'listi', color: true, defaultVerbosity: opts.verbosity, colorMap: { listi: '\x1b[36m' } });
+
+log(1)('Options', opts);
+
+require('./listi').init(opts);
---
+++ src/server/listi.js
@@ -0,0 +1,79 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const log = new (require('log'))({ tag: 'listi' });
+const SocketServer = require('websocket-server');
+
+const listi = {
+	init: function(opts){
+		this.opts = opts;
+
+		this.rootPath = function rootPath(){ return path.join(opts.rootFolder, ...arguments); };
+
+		this.lists = new (require('config-manager'))(path.join(os.homedir(), '.listi.json'));
+
+		const fontAwesomePath = this.rootPath('node_modules/@fortawesome/fontawesome-free/webfonts');
+		const { app, staticServer } = require('http-server').init(opts.port, opts.rootFolder);
+
+		this.socketServer = new SocketServer({ server: app.server });
+
+		app.use('/resources', staticServer(path.join(opts.rootFolder, 'client/resources')));
+		app.use('/fonts', staticServer(path.join(opts.rootFolder, 'client/fonts')));
+
+		if(fs.existsSync(fontAwesomePath)) app.use('/webfonts', staticServer(fontAwesomePath));
+
+		app.get('/home', (req, res, next) => { res.sendPage('index'); });
+
+		this.socketServer.registerEndpoints(this.socketEndpoints);
+
+		log.info('Initialized');
+	},
+	socketEndpoints: {
+		client_connect: function(){
+			log('Client connected');
+
+			this.reply('lists', Object.keys(listi.lists.current));
+		},
+		lists: function(){
+			log('Requested lists');
+
+			this.reply('lists', Object.keys(listi.lists.current));
+		},
+		list: function(name){
+			log(`Requested list: ${name}`);
+
+			this.reply('list', { name, arr: listi.lists.current[name] });
+		},
+		list_edit: function(list){
+			log(`${list.name ? (list.delete ? 'Delete' : 'Edit') : 'Create'} list: ${list.name || list.new.name}`);
+
+			if(!list.name) listi.lists.current[list.new.name] = [];
+
+			else {
+				if(!list.delete) listi.lists.current[list.new.name] = listi.lists.current[list.name];
+
+				delete listi.lists.current[list.name];
+			}
+
+			if(listi.opts.persistent) listi.lists.save();
+
+			this.reply('lists', Object.keys(listi.lists.current));
+		},
+		list_item_edit: function(item){
+			log(`${typeof item.index === 'number' ? (item.delete ? 'Delete' : 'Edit') : 'Create'} list item: ${item.summary || (item.new && item.new.summary) || item.index}`);
+
+			if(item.delete) listi.lists.current[item.listName].splice(item.index, 1);
+
+			else if(typeof item.index === 'number') listi.lists.current[item.listName][item.index] = item.new;
+
+			else listi.lists.current[item.listName].push(item.new);
+
+			if(listi.opts.persistent) listi.lists.save();
+
+			this.reply('list', { name: item.listName, arr: listi.lists.current[item.listName] });
+		}
+	}
+};
+
+module.exports = listi;
