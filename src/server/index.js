4181420633b921971031fa46a479b80d919944aa
justfatlard <fatlard1993@gmail.com>
2021-01-26 13:55:29 -0800

    swap out yargs for argi

--- src/server/index.js
+++ src/server/index.js
@@ -1,44 +1,29 @@
 #!/usr/bin/env node
 
-const path = require('path');
+const argi = require('argi');
 
-const yargs = require('yargs');
-
-yargs.parserConfiguration({
-	'camel-case-expansion': false
+argi.parse({
+	verbosity: {
+		type: 'int',
+		alias: 'v',
+		defaultValue: 1
+	},
+	port: {
+		type: 'int',
+		alias: 'p',
+		defaultValue: 80
+	},
+	persistent: {
+		type: 'boolean',
+		alias: 'P',
+		defaultValue: true,
+		description: 'Save lists to a file'
+	}
 });
 
-yargs.alias({
-	h: 'help',
-	v: 'verbosity',
-	ver: 'version',
-	p: 'port',
-	P: 'persistent'
-});
+const options = argi.options.named;
+const log = new (require('log'))({ tag: 'listi', color: true, defaultVerbosity: options.verbosity, colorMap: { listi: '\x1b[36m' } });
 
-yargs.boolean(['h', 'ver', 'persistent']);
+log(1)('Options', options);
 
-yargs.default({
-	v: 1,
-	p: 8080,
-	P: true
-});
-
-yargs.describe({
-	h: 'This',
-	v: '<level>',
-	p: '<port>',
-	P: 'Saves lists to file'
-});
-
-const args = yargs.argv;
-
-['_', '$0', 'v', 'p', 'P'].forEach((item) => { delete args[item]; });
-
-const opts = Object.assign(args, { args: Object.assign({}, args), verbosity: Number(args.verbosity) });
-
-const log = new (require('log'))({ tag: 'listi', color: true, defaultVerbosity: opts.verbosity, colorMap: { listi: '\x1b[36m' } });
-
-log(1)('Options', opts);
-
-require('./listi').init(opts);
+require('./listi').init(options);
