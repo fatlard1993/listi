4b0a94b0acc650e7a078ed2c590148ccb1f6f427
justfatlard <fatlard1993@gmail.com>
2021-11-14 01:33:09 -0800

    Fix built server

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const { app, staticServer } = require('http-server');
 
 const { rootPath } = require('./listi');
 
-app.use(staticServer(rootPath('dist')));
+app.use(staticServer(rootPath('client/dist')));
 
 app.use((req, res, next) => {
 	next(res.reqType === 'file' ? { code: 404, detail: `Could not find ${req.originalUrl}` } : null);
